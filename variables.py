c1fba46e6ea5fff0bfb45535c629c8d854803a15
krpavi <155173494+krpavi@users.noreply.github.com>
2024-01-05 16:35:57 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,22 @@
+#student's detail
+student_name = "Pavithran"
+student_id = 13579
+student_height = 175.5
+student_weight = 67.5
+marks_in_English = 93
+marks_in_Maths = 97
+marks_in_Science = 95
+marks_in_CS = 96
+student_total = 381
+student_rank = 3
+
+print(student_name)
+print(student_id)
+print(student_height)
+print(student_weight)
+print(marks_in_English)
+print(marks_in_Maths)
+print(marks_in_Science)
+print(marks_in_CS)
+print(student_total)
+print(student_rank)
